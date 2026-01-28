44e4671014e2c6eb3de8df92ccb0f88142b91217
caner.karadag <karadag.cnr@gmail.com>
2026-01-28 16:40:43 +0300

    Remove vercel.json and update for PostgreSQL deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "605c356579bf4b07"; }
+/******/ 		__webpack_require__.h = function() { return "c67bfe2e6fc90983"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
